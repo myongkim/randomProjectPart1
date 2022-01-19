66447661c26ff363dde2cb47bedb85f9f9cfd811
Isaac Kim <myong328@gmail.com>
2022-01-19 12:56:52 -0800

    Initial Commit

---
+++ randomProjectPart1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  randomProjectPart1
+//
+//  Created by Isaac Kim on 1/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
